9e0756b7f31c1d7635b5877fc0474803eb21c727
Yohan Wiputra <yowiputra@gmail.com>
2017-07-20 20:32:04 +0000

    added map initialization and click functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,64 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+$(document).ready(function(){
+  function initMap() {
+    // For creating content of infoWindow
+    function createWindowContent() {
+      return `<div>
+      <form id="info_window_input" action="/list" method="post">
+          <textarea class="test" name="text"></textarea>
+          <input type="submit" value="Submit">
+        </form>
+      </div>`;
     }
-  });;
+
+    //For adding marker
+    function addMarker(point){
+      var marker = new google.maps.Marker({
+        position:point,
+        map:map
+      });
+      var infoWindow = new google.maps.InfoWindow();
+      google.maps.event.addListener(marker, 'click', function(event){
+        infoWindow.setContent(createWindowContent());
+        infoWindow.open(map, marker)
+      });
+    }
+
+    // fake JSON response
+    var markers = [
+      {
+        // Miku
+        coords: {lat: 49.2803495, lng: -123.112195},
+      },
+      {
+        // Sushi California
+        coords: {lat:49.2630685, lng: -123.1147977},
+      },
+      {
+        // Yamato Sushi
+        coords: {lat: 49.2766505, lng: -123.1278187},
+      }
+    ];
+
+    // map options
+    var options = {
+      zoom:13,
+      center:{lat: 49.2827, lng: -123.1207}
+    };
+
+    // initiate new map
+    var map = new google.maps.Map(document.getElementById('map'), options);
+
+    // loop through JSON array to add marker
+    markers.forEach(function(obj){
+      addMarker(obj.coords);
+    })
+
+    // add listener for clicks on map
+    google.maps.event.addListener(map, 'click', function(event){
+      console.log(event.latLng.lat(), event.latLng.lng());
+      addMarker({lat: event.latLng.lat(), lng: event.latLng.lng()});
+    });
+  }
+
+  initMap();
 });
